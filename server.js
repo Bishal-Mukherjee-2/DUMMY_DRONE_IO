272356dcfe5aa66bad88b5eeddcc378feec0bfa5
Bishal-Mukherjee-2 <96591759+Bishal-Mukherjee-2@users.noreply.github.com>
2022-02-24 23:48:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 
 console.log("I'm in server.js");
 console.log("I'm in server.js");
+console.log("I'm written from github");
 app.listen(8080, () => {
   console.log("SERVER WORKING");
 });
