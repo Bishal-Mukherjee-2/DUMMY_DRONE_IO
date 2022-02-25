cd243204eb8a11f08292966e017c3754ccdb4ec4
Bishal Mukherjee <bishal.mukherjee@shoshinschool.com>
2022-02-26 01:32:54 +0530

    .drone.yml file modified10

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 
 console.log("I'm in server.js");
 console.log("I'm in server.js");
+console.log("drone.io working");
 app.listen(8080, () => {
   console.log("SERVER WORKING");
 });
